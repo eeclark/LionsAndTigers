1bd9ca2c48172e54107c7c00c58e6e2531aac8a6
Edward Clark <eeclark@gmail.com>
2015-01-21 19:46:08 -0600

    added randomizer

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Edward Clark on 1/21/15.
+//  Copyright (c) 2015 Edward Clark. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"default.jpg")
+}
